0754953d8237cdf6bd46cb44a7ce4f11d8e22782
KaisoBits <31374466+KaisoBits@users.noreply.github.com>
2024-09-04 22:36:42 +0200

    Add mouse movement

--- WaveFunctionCollapseTest/Program.cs
+++ WaveFunctionCollapseTest/Program.cs
@@ -18,9 +18,54 @@ Tilemap tilemap = new(tex, atlasSize, gridSize, clock);
 window.SetKeyRepeatEnabled(true);
 window.Resized += (s, e) => window.SetView(new View(new Vector2f(), new Vector2f(e.Width, e.Height)));
 window.Closed += (s, e) => window.Close();
+
+bool isMovingCam = false;
+Vector2f lastPos = new();
+float zoom = 1.0f;
+
+window.Resized += (s, e) => view.Size = new Vector2f(e.Width, e.Height) * zoom;
+
+window.MouseWheelScrolled += (s, e) =>
+{
+    float multiplier = Math.Abs(e.Delta);
+    float ratio = e.Delta < 0 ? 1.25f * multiplier : 0.8f / multiplier;
+    zoom = Math.Clamp(zoom * ratio, 0.2f, 3.0f);
+    view.Size = (Vector2f)window.Size * zoom;
+};
+
+window.MouseButtonPressed += (s, e) =>
+{
+    if (e.Button != Mouse.Button.Left)
+        return;
+
+    isMovingCam = true;
+    lastPos = new Vector2f(e.X, e.Y);
+};
+
+window.MouseButtonReleased += (s, e) =>
+{
+    if (e.Button != Mouse.Button.Left)
+        return;
+
+    isMovingCam = false;
+};
+
+window.MouseMoved += (s, e) =>
+{
+    if (!isMovingCam)
+        return;
+
+    Vector2f currentPos = new Vector2f(e.X, e.Y);
+
+    Vector2f offset = (lastPos - currentPos) * zoom;
+
+    view.Move(offset);
+
+    lastPos = currentPos;
+};
+
 window.KeyPressed += (s, e) =>
 {
-
     if (e.Code == Keyboard.Key.Space)
     {
         if (e.Control)
@@ -36,12 +81,26 @@ window.KeyPressed += (s, e) =>
 
     if (e.Code == Keyboard.Key.R)
         tilemap = new(tex, atlasSize, gridSize, clock);
+
+
+    if (e.Code == Keyboard.Key.PageDown)
+    {
+        view.Zoom(1.25f);
+        window.SetView(view);
+    }
+    else if (e.Code == Keyboard.Key.PageUp)
+    {
+        view.Zoom(0.8f);
+        window.SetView(view);
+    }
 };
 
 while (window.IsOpen)
 {
     window.Clear(new Color(135, 206, 235));
     window.DispatchEvents();
+    window.SetView(view);
+
 
     window.Draw(tilemap);
 
